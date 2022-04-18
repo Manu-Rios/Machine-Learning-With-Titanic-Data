a7a016cf506e708ed08a62a003169341d2ac8161
Manu-Rios <riosmanuel.07@gmail.com>
2022-04-18 14:59:47 +0200

    correction on add function

--- Git_test.py
+++ Git_test.py
@@ -1,6 +1,8 @@
 #I need to implement this:
 def add(x,y):
-    return x+y
+    if ( type(x)==int & type(y) == int)
+        return x+y
+    else return None
   
 #Bob need to implement this:
 def mult (x,y):
